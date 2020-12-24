19cd7ca3771477be8e5c959bb5760533f126d775
Jacob de Vos <jacobdevos@hotmail.com>
2020-12-23 19:02:26 -0500

    adding post, sample windows curl string: "curl --request POST --header "Content-Type: application/json" --data "{\"name\":\"val\"}" http://localhost:5001/jake"

--- OverwatchStats/OverwatchStats.py
+++ OverwatchStats/OverwatchStats.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def hello():
-    return "hello world!"
+    if request.json:
+        return "Hello Jake \n {}".format(request.json)
+    else:
+        return "hello world!"
+@app.route("/jake", methods=['POST'])
+def jake():
+    if request.json:
+        return request.json
+    else:
+        return "wut?"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
